eb8f9854f6db51a77b1dc8f2d75701502832f2ba
EForestier <edouard.forestier@stfelixlasalle.fr>
2017-03-25 12:49:20 +0100

    stock

---
+++ stock_restauration/stock_restauration/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using MahApps.Metro.Controls;
+
+namespace stock_restauration
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow :MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_benevole_Click(object sender, RoutedEventArgs e)
+        {
+            benevole wind_bene = new benevole();
+            wind_bene.ShowDialog(); 
+        }
+
+        private void btn_organisateur_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ stock_restauration/stock_restauration/tab_benevole.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+using MahApps.Metro.Controls;
+
+namespace stock_restauration
+{
+    /// <summary>
+    /// Logique d'interaction pour tab_benevole.xaml
+    /// </summary>
+    public partial class tab_benevole : MetroWindow
+    {
+        public tab_benevole()
+        {
+            InitializeComponent();
+        }
+    }
+}
